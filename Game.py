07b3ca8c495fcc9154f3d221bac976032c3343f1
TABLET-7JR9V15O\finba <fhachambers@gmail.com>
2022-04-27 15:57:44 +0100

    Lesson 1

--- Game.py
+++ Game.py
@@ -3,19 +3,29 @@
 
 class Game:
 
+    EMPTY = " "
+    p1 = "X"
+    p2 = "O"
+
     def __init__(self):
-        pass
+        self.__board = [[Game.EMPTY for _ in range(3)] for _ in range(3)]
+        self.__player = Game.p1
 
     def __repr__(self):
-        pass
+        return "Current Board:"
 
     def play(self,row,col):
-        pass
+        row -= 1 # 0-basing
+        col -= 1
+        self.__board[row][col] = self.__player
+        if self.__player == Game.p1:
+            self.__player = Game.p2
+        else:
+            self.__player = Game.p1
     
     @property
     def winner(self):
-        pass
+        return None
 
 if __name__ == "__main__":
-    t = Terminal()
-    t.run
+    pass
